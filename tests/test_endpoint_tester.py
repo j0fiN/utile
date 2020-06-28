69cc6314c37bdbddd9f85515a2c78caba7e3dcee
j0fiN <jofinfab@gmail.com>
2020-06-28 20:37:09 +0530

    TEST:remove pytest due to crashes

--- tests/test_endpoint_tester.py
+++ tests/test_endpoint_tester.py
@@ -1,19 +1,8 @@
 import unittest
-import requests
-response = requests.request("GET", url="https://covid-api-19.glitch.me/api/notildore2020/country/India")
-print(response.raw)
-print(response.reason)
-print(response.request)
-print(response.text)
-print(response.url)
-print(response.apparent_encoding)
-print(response.history)
-print(response.cookies)
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(False, False)
 
-# class MyTestCase(unittest.TestCase):
-#     def test_something(self):
-#         self.assertEqual(True, False)
-#
-#
-# if __name__ == '__main__':
-#     unittest.main()
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/test_timer.py
+++ tests/test_timer.py
@@ -1,7 +1,6 @@
 import unittest
 import time
 import sys
-import pytest
 sys.path.append('..')
 from Utile.Timer import timer
 
@@ -9,22 +8,20 @@ from Utile.Timer import timer
 class MyTestCase(unittest.TestCase):
 
     def test_returns_1(self):
-        self.sample_2()
-        self.assertEqual(None, self.sample_2())
+        # self.sample_2()
+        self.assertAlmostEqual(0.5, self.sample_2()[1], 1)
 
     def test_returns_2(self):
         result = self.sample_1()[1]
         self.assertAlmostEqual(0.5, result, 1)
 
-    def test_doc(self):
-        timer.__doc__ # pseudo code
 
     @timer(True)
     def sample_1(self):
         time.sleep(0.5)
         return None
 
-    @timer(store=False)
+    @timer(store=True)
     def sample_2(self):
         time.sleep(0.5)
         return None
