e8129681afc24b447ca3206cbf53ab28995196e6
j0fiN <jofinfab@gmail.com>
2020-06-28 12:50:40 +0530

    MAINT:put all code into src

--- src/Utile/Threader.py
+++ src/Utile/Threader.py
@@ -4,18 +4,22 @@ from concurrent.futures import ThreadPoolExecutor
 from Timer import timer
 
 
-def threader(funcs):  # {add():[1, 2, 3]}
+def threader(funcs):  # {add():[[1, 2, 3], [1, 2, 3]]}
 
     def th(func):
 
         @wraps(func)
         def wrapper(*args, **kwargs):
-            with ThreadPoolExecutor(max_workers=len(funcs.keys())) as exe:
+            workers = 0
+            for arg in funcs.values(): workers = workers + len(arg)
+
+            with ThreadPoolExecutor(max_workers=workers) as exe:
                 def processes():
                     for (i, j) in zip(funcs.keys(), funcs.values()):
-                        yield exe.submit(i, *j)
+                        for k in j:
+                            yield exe.submit(i, *k)
 
-                #print(list(processes()))
+                # print(list(processes()))
                 results = list()
                 for process in processes():
                     try:
@@ -24,16 +28,14 @@ def threader(funcs):  # {add():[1, 2, 3]}
                     except TimeoutError:
                         raise Exception("Time Limit Exceeded...")
             return results
-
         return wrapper
-
     return th
 
 
 if __name__ == '__main__':
+
+
     @timer(store=False)
-    @threader({pow: [123, 321]})
-    def foo():pass
-
-
-    print(len(foo()))
+    @threader({pow: [[123, 321] for _ in range(10000)]})
+    def foo(): pass
+    foo()
