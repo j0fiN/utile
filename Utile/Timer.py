12f996e635ebed3cb54eb7485fcb8fafd3c7f8e3
j0fiN <jofinfab@gmail.com>
2020-06-29 22:57:05 +0530

    DOC: updated doc of timer decorator

--- Utile/Timer.py
+++ Utile/Timer.py
@@ -1,24 +1,27 @@
+import time
+from functools import wraps
 
-"""
-Utility on time.
-"""
-def timer(store=False, roundoff=10):
+
+def timer(store=False, round_off=10):
     """
     Decorator which print execution time of any function.
-    Can be used to measure the response time of a http request.
 
-    :param store: do want to store your execution time(True) or just print it(default=False).
-    :param roundoff: decimals to round off the execution time (default is 10).
-    if store is true you will get a tuple as return type in your function.
-    Usage:
+    Arguments:
+        store:  storing your execution time(True) or just print it(default=False).
+        round_off: decimals to round off the execution time (default is 10).
 
-    @timer()
-    def foo():
-        v_foo = "Hello"
-        return v_foo + "World"
+    Returns:
+        Tuple containing return value and the execution time if store=True.
+        Return value if store=False
+
+    Example:
+        from Utile.Timer import timer
+        import time
+        @timer()
+        def foo():
+            time.sleep(1) # function sleeping for 1 second
+        foo()
     """
-    import time
-    from functools import wraps
     def inner_timer(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
@@ -27,12 +30,14 @@ def timer(store=False, roundoff=10):
             finish_time = time.time()
             if store is False:
                 print('=' * 100)
-                print(f"{func.__name__} function executed in : %s s" % round((finish_time - start_time), roundoff))
+                print(f"{func.__name__} function executed in : %s s" % round((finish_time - start_time), round_off))
                 print('=' * 100)
                 print('\n')
                 return value
             else:
-                exec_time = round((finish_time - start_time), roundoff)
+                exec_time = round((finish_time - start_time), round_off)
                 return value, exec_time
+
         return wrapper
+
     return inner_timer
