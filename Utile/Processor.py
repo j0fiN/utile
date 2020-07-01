3fe20f33c9e9222056204c265737e01863636e01
j0fiN <jofinfab@gmail.com>
2020-07-01 11:07:18 +0530

    ADD: added processor decorator

--- Utile/Processor.py
+++ Utile/Processor.py
@@ -1,79 +1,52 @@
-from concurrent.futures import ProcessPoolExecutor, as_completed
-from Timer import timer
-from functools import wraps
-import time
 
+
+from Timer import timer
 
 def processor(funcs, inresult=False): # {add():[[1, 2, 3], [1, 2, 3]]}
-
+    from multiprocessing import Pool
+    import os
+    from functools import wraps
+    import time
     def proc(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            workers = 0
-            for arg in funcs.values(): workers = workers + len(arg)
-
-
             if __name__ == '__main__':
-                with ProcessPoolExecutor() as exe:
+                with Pool(int(os.environ["NUMBER_OF_PROCESSORS"])) as exe:
+                    s = time.time()
                     processes = list()
                     for (i, j) in zip(funcs.keys(), funcs.values()):
-                        for k in j:
-                            if str(type(k)) == "<class 'dict'>":
-                                processes.append(exe.submit(i, **k))
-                            else:
-                                processes.append(exe.submit(i, *k))
-                    result = list()
-                    for process in as_completed(processes):
-                        result.append(process.result())
-
+                        processes.append(exe.starmap_async(i, j))
+                    f = time.time()
+                    print("= "*6, f-s)
                     if inresult is False:
-                        return result
+                        return processes
                     else:
-                        return func(*args, **kwargs), result
+                        return func(*args, **kwargs), processes
         return wrapper
     return proc
 
 
-def manyprocessor(funcs): # {funcs:[[], [], []]}
 
-    def mproc(func):
-
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            with ProcessPoolExecutor() as exe:
-                processes = list()
-                results = list()
-                s = time.time()
-                arg = funcs[list(funcs.keys())[0]]
-                for i in arg:
-                    processes.append(exe.submit(list(funcs.keys())[0], *i))
-                for process in processes:
-                    results.append(process.result())
-                f = time.time()
-                print(results, " = ", f-s)
-                return results
-        return wrapper
-    return mproc
 
 
 
 
 
 if __name__ == '__main__':
+    def foo(a, b):
+        return pow(a, b)
 
-    @timer()
-    @manyprocessor({pow:[[123, 321] for _ in range(1000)
-                         ]})
-    def foo(): pass
-    foo()
+
+    @processor({pow: [[123, 321] for _ in range(10)]})
+    def foo_2():
+        pass
+    print(foo_2())
 
     @timer()
     def fooer():
-        for i in range(1000):
+        for i in range(10):
             pow(123, 321)
     fooer()
-    # import os
-    # print(int(os.environ["NUMBER_OF_PROCESSORS"]))
 
 
 
