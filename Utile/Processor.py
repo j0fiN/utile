76e4b8bbf273c35d7c94f979fb0377fe92110326
j0fiN <jofinfab@gmail.com>
2020-07-01 11:54:09 +0530

    DEV: processor made proper :sparkle:

--- Utile/Processor.py
+++ Utile/Processor.py
@@ -2,7 +2,7 @@
 
 from Timer import timer
 
-def processor(funcs, inresult=False): # {add():[[1, 2, 3], [1, 2, 3]]}
+def processor(funcs, inresult=False, get_result=False): # {add():[[1, 2, 3], [1, 2, 3]]}
     from multiprocessing import Pool
     import os
     from functools import wraps
@@ -15,7 +15,11 @@ def processor(funcs, inresult=False): # {add():[[1, 2, 3], [1, 2, 3]]}
                     s = time.time()
                     processes = list()
                     for (i, j) in zip(funcs.keys(), funcs.values()):
-                        processes.append(exe.starmap_async(i, j))
+                        if get_result is False:
+                            processes.append(exe.starmap_async(i, j))
+                        else:
+                            if __name__ == "__main__":
+                                processes.append(exe.starmap_async(i, j).get())
                     f = time.time()
                     print("= "*6, f-s)
                     if inresult is False:
@@ -29,22 +33,20 @@ def processor(funcs, inresult=False): # {add():[[1, 2, 3], [1, 2, 3]]}
 
 
 
-
+def foo(a, b):
+    return pow(a, b)
 
 
 if __name__ == '__main__':
-    def foo(a, b):
-        return pow(a, b)
-
-
-    @processor({pow: [[123, 321] for _ in range(10)]})
+    @processor({foo: [[123, 321] for _ in range(1000)],
+                pow:[[123, 321] for i in range(10)]}, get_result=False)
     def foo_2():
         pass
     print(foo_2())
 
     @timer()
     def fooer():
-        for i in range(10):
+        for i in range(1000):
             pow(123, 321)
     fooer()
 
