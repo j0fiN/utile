480afd669277b6ca55a01e454d9920df584bbbf6
j0fiN <jofinfab@gmail.com>
2020-07-01 12:41:24 +0530

    FIX:major bug fix in development and doc

--- Utile/Processor.py
+++ Utile/Processor.py
@@ -1,58 +1,62 @@
+def processor(funcs, func_result=False, get_result=False):
+    """
+    A Frame-Determined decorator to spring up number of CPU bound tasks.
+    
+    Arguments:
+        funcs: dictionary holding all your tasks in form of {my_function:[list of parameters]}.
+        func_result: type:boolean True to return function's return value (default=False).
+        get_result: type:boolean True to return the MapResult object values (process becomes little slow)(default=False)
+        
+    Returns:
+        return a list of MapResult object(s)(with values if get_result=True) of all the CPU bound 
+        tasks(specified in decorator) if func_result = False. Otherwise returns a tuple containing return value(s) 
+        and the list respectively.
+        
+    Examples:
+        from Utile.Processor import processor
 
 
-from Timer import timer
+        def power(a, b):
+            return pow(a, b)  # a sample method for CPU  bound task
 
-def processor(funcs, inresult=False, get_result=False): # {add():[[1, 2, 3], [1, 2, 3]]}
+
+        if __name__ == "__main__":  # important to ensure this.
+            @processor({power: [[123, 321] for _ in range(1000)]})
+            def foo(): pass
+            print(foo())
+    """
     from multiprocessing import Pool
     import os
     from functools import wraps
-    import time
+
     def proc(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            if __name__ == '__main__':
+            if __name__ == 'Utile.Processor' or __name__ == 'Processor':
                 with Pool(int(os.environ["NUMBER_OF_PROCESSORS"])) as exe:
-                    s = time.time()
                     processes = list()
                     for (i, j) in zip(funcs.keys(), funcs.values()):
                         if get_result is False:
                             processes.append(exe.starmap_async(i, j))
                         else:
-                            if __name__ == "__main__":
-                                processes.append(exe.starmap_async(i, j).get())
-                    f = time.time()
-                    print("= "*6, f-s)
-                    if inresult is False:
+                            processes.append(exe.starmap_async(i, j).get())
+                    if func_result is False:
                         return processes
                     else:
                         return func(*args, **kwargs), processes
+
         return wrapper
+
     return proc
 
 
+def power(a, b):
+    return pow(a, b)  # a sample method for CPU  bound task
 
 
-
-def foo(a, b):
-    return pow(a, b)
-
-
-if __name__ == '__main__':
-    @processor({foo: [[123, 321] for _ in range(1000)],
-                pow:[[123, 321] for i in range(10)]}, get_result=False)
-    def foo_2():
-        pass
-    print(foo_2())
-
-    @timer()
-    def fooer():
-        for i in range(1000):
-            pow(123, 321)
-    fooer()
+if __name__ == "__main__":  # important to ensure this.
+    @processor({power: [[123, 321] for _ in range(1000)]})
+    def foo(): pass
 
 
-
-
-
-
-
+    print(foo())
