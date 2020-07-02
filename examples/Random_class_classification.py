9d7e201c7c67d37d04296e8f59d5753cb9c9b809
j0fiN <jofinfab@gmail.com>
2020-07-02 21:34:53 +0530

    ADD: example-2

--- examples/Random_class_classification.py
+++ examples/Random_class_classification.py
@@ -9,8 +9,10 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 
 import sys
+
 sys.path.append("..")
-from Utile.Processor import processor # noqa
+from utile.Processor import processor  # noqa
+from utile.Timer import timer  # noqa
 
 data_frame = load_iris()
 input_data = data_frame.data
@@ -23,6 +25,47 @@ model_rfc = RandomForestClassifier(n_estimators=10)
 model_gnb = GaussianNB()
 model_mnb = MultinomialNB()
 
-# @
-# def fitter():
-#     pass
+
+def knc_modeler():
+    model_knc.fit(X_train, y_train)
+    print(model_knc.score(X_test, y_test))
+
+
+def svc_modeler():
+    model_svc.fit(X_train, y_train)
+    print(model_svc.score(X_test, y_test))
+
+
+def rfc_modeler():
+    model_rfc.fit(X_train, y_train)
+    print(model_rfc.score(X_test, y_test))
+
+
+def gnb_modeler():
+    model_gnb.fit(X_train, y_train)
+    print(model_gnb.score(X_test, y_test))
+
+
+@timer()
+@processor({knc_modeler: [[]],
+            svc_modeler: [[]],
+            rfc_modeler: [[]],
+            gnb_modeler: [[]]}, get_result=True)
+def fitter():
+    pass
+
+
+@timer()
+def brute_force():
+    brute_result = list()
+    models = [KNeighborsClassifier(n_neighbors=5), SVC(), RandomForestClassifier(n_estimators=10), GaussianNB()]
+    for mod in models:
+        model = mod
+        model.fit(X_train, y_train)
+        brute_result.append(model.score(X_test, y_test))
+    return brute_result
+
+
+if __name__ == '__main__':
+    result = fitter()
+    print(brute_force())
