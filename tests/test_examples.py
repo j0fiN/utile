9fcf1cd7787f8c71ac843b015feb70f1394b1aeb
j0fiN <jofinfab@gmail.com>
2020-07-03 13:19:43 +0530

    TEST: for examples

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,14 +1,14 @@
 import unittest
 import sys
 import time
-import requests
 sys.path.append("..")
 from examples.GET_requester import base, get_requester # noqa
+from examples.iris_flower_classification import knc_modeler # noqa
 
 
 class MyTestCase(unittest.TestCase):
 
-    def test_example_base(self):
+    def test_example_1_base(self):
         s = time.time()
         result = base()
         f = time.time()
@@ -17,6 +17,18 @@ class MyTestCase(unittest.TestCase):
 
     def test_example_1_requester(self):
         self.assertEqual(200, get_requester('https://unsplash.com/photos/A-NVHPka9Rk/download?force=true&w=640'))
+
+    def test_example_2_kns(self):
+        self.assertGreater(kns_modeler(), 0.85)
+
+    def test_example_2_svc(self):
+        self.assertGreater(svc_modeler(), 0.85)
+
+    def test_example_2_rfc(self):
+        self.assertGreater(rfc_modeler(), 0.85)
+
+    def test_example_2_gnb(self):
+        self.assertGreater(gnb_modeler(), 0.85)
 
 
 if __name__ == '__main__':
