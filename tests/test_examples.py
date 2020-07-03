a2aefd52d0e59c1a947c21c92ea1000d04254047
j0fiN <jofinfab@gmail.com>
2020-07-03 13:58:42 +0530

    ADD:add example-2

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -3,7 +3,7 @@ import sys
 import time
 sys.path.append("..")
 from examples.GET_requester import base, get_requester # noqa
-from examples.iris_flower_classification import knc_modeler # noqa
+from examples.iris_flower_classification import knc_modeler, gnb_modeler, svc_modeler, rfc_modeler, fitter # noqa
 
 
 class MyTestCase(unittest.TestCase):
@@ -18,8 +18,8 @@ class MyTestCase(unittest.TestCase):
     def test_example_1_requester(self):
         self.assertEqual(200, get_requester('https://unsplash.com/photos/A-NVHPka9Rk/download?force=true&w=640'))
 
-    def test_example_2_kns(self):
-        self.assertGreater(kns_modeler(), 0.85)
+    def test_example_2_knc(self):
+        self.assertGreater(knc_modeler(), 0.85)
 
     def test_example_2_svc(self):
         self.assertGreater(svc_modeler(), 0.85)
@@ -29,6 +29,9 @@ class MyTestCase(unittest.TestCase):
 
     def test_example_2_gnb(self):
         self.assertGreater(gnb_modeler(), 0.85)
+
+    def test_example_2_fitter(self):
+        self.assertEqual(str(type(fitter())), "<class 'list'>")
 
 
 if __name__ == '__main__':
