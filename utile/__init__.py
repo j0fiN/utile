9280d0da68ce8bd4c6d4a7e4e0596921fe932927
j0fiN <jofinfab@gmail.com>
2020-07-04 19:56:27 +0530

    TODO:add doc link

---
+++ utile/__init__.py
@@ -0,0 +1,14 @@
+"""
+The python package which eases your <codeflow> using @decorators.
+
+@timer(store=False, round_off=10)
+    Decorator which print execution time of any function.
+
+@threader(funcs, func_result=False)
+    A Frame-Determined decorator to spring up number of I/O bound tasks.
+
+@processor(funcs, func_result=False, get_result=False)
+    A Frame-Determined decorator to spring up number of CPU bound tasks.
+
+For more info: https://
+"""
