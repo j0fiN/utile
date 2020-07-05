042f24fc871af50e0be8e17522db2afd94d7b7b2
j0fiN <jofinfab@gmail.com>
2020-07-05 13:41:32 +0530

    ADD: package deployment

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="utile", # Replace with your own username
+    name="utile",  # Replace with your own username
     version="v1.0",
     author="Jofin F Archbald",
     author_email="jofinfab@gmail.com",
@@ -13,6 +13,11 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/j0fiN/utile.git",
     packages=setuptools.find_packages(),
+    project_urls={
+        'Documentation': 'https://utile.readthedocs.io/',
+        'Say Thanks!': 'http://j0fiN.github.io/home/',
+        'Source': 'https://github.com/j0fiN/utile/',
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
