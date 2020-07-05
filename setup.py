a105886ec5739d85e21939d118010297b0d79209
j0fiN <jofinfab@gmail.com>
2020-07-05 11:43:07 +0530

    ADD: documentation build

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="utile", # Replace with your own username
+    version="v1.0",
+    author="Jofin F Archbald",
+    author_email="jofinfab@gmail.com",
+    description="The python package which eases your <codeflow> using decorators",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/j0fiN/utile.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
