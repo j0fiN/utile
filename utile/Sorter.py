090d2662d306ba16bfa705341bc903ae51707093
j0fiN <jofinfab@gmail.com>
2020-08-28 13:00:30 +0530

    DEV: rust bindings for timsort

---
+++ utile/Sorter.py
@@ -0,0 +1,18 @@
+import rusty_sorter as so
+
+
+def sort(arr: list) -> list:
+    """
+    Timsort built in rust.
+    """
+    if str(type(arr[0])) == "<class 'int'>":
+        return so.sort_int(arr)
+    elif str(type(arr[0])) == "<class 'float'>":
+        return so.sort_float(arr)
+    else:
+        return so.sort_str(arr)
+
+
+if __name__ == '__main__':
+    arrr = ["ap", "ca", "aap"]
+    print(sort(arrr))
