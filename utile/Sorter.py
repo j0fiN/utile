8ef90a95583a3030b95493e3a2db37722a68e298
j0fiN <jofinfab@gmail.com>
2020-09-07 13:30:49 +0530

    DOC: doc update, author and unicode

--- utile/Sorter.py
+++ utile/Sorter.py
@@ -1,10 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Author : JOFIN F ARCHBALD
+"""
 
 
 def sort(arr: list) -> list:
-    import rusty_sorter as so
     """
     Timsort built in Rust.
     """
+    import rusty_sorter as so
     if str(type(arr[0])) == "<class 'int'>":
         return so.sort_int(arr)
     elif str(type(arr[0])) == "<class 'float'>":
@@ -14,4 +18,5 @@ def sort(arr: list) -> list:
 
 
 if __name__ == '__main__':
-    print(sort([1, 3, 4, 2, 5]))
+    # print(sort([1, 3, 4, 2, 5]))
+    pass
