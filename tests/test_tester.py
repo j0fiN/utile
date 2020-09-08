abae72b70714f7a66ee98dd440715ec14a7aa66e
j0fiN <jofinfab@gmail.com>
2020-09-08 08:17:29 +0530

    TEST: Tester.py

---
+++ tests/test_tester.py
@@ -0,0 +1,22 @@
+import unittest
+import sys
+
+sys.path.append('..')
+from utile import * # noqa
+
+
+class TestTester(unittest.TestCase):
+
+    @tester(return_test={
+        'value': 42,
+        'type': int
+    })
+    def sample_tester_int(self, **_):
+        return 42
+
+    def test_tester_1(self):
+        self.assertEqual(self.sample_tester_int(test=True)[1], {'value': 42, 'type': int})
+
+
+if __name__ == '__main__':
+    unittest.main()
