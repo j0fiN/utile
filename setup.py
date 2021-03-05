f28b15e74b98050f5c9f38be1dc28313e0b554c4
Jofin <jofinfab@gmail.com>
2021-03-05 17:15:12 +0530

    FIX: fix issue with *.pyd

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("readmepy.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="utile",  # Replace with your own username
-    version="v1.1.0",
+    version="v1.1",
     author="Jofin F Archbald",
     author_email="jofinfab@gmail.com",
     description="The python package which eases your <codeflow> using decorators",
@@ -18,6 +18,7 @@ setuptools.setup(
         'Say Thanks!': 'https://jofin-f-archbald.herokuapp.com/',
         'Source': 'https://github.com/j0fiN/utile/',
     },
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
